95380174d2972450d767760ef9df4de5dea2cf17
Monty3141 <juanpablomontoyae@gmail.com>
2019-12-01 19:54:30 -0700

    Cambios sonido agregado

--- src/App.js
+++ src/App.js
@@ -7,19 +7,18 @@ import ToggleCount from './Components/Autonomous/ToggleCount.js'
 import Timer from './Components/Timer/timer.js'
 
 function App() {
-  console.log(document.getElementsByTagName('p'));
-  
+ 
   return (
     <div className="App" >
         <div style = {{textAlign : 'center', width:  '100%', height: '20vh'}}>
 
-        <Timer tiempo = {180}/>
+        <Timer driver= {180} endgame = {30} lim ={0} tiempo = {210}/>
         </div>
         
-        <div style = {{float: 'left',display : 'inline-block', width: '30%', height:'80%'}}>
+        <div id = 'autodiv' style = {{float: 'left',display : 'inline-block', width: '30%', height:'80%'}}>
 
-        <h1>Autonomous</h1>
-        <div style = {{display : 'inline-block'}}>
+        <h1 id = 'auto'>Autonomous</h1>
+        <div  style = {{display : 'inline-block'}}>
           <Counter points = {4} tipo = 'Placing Stones' />
           <Counter points = {2} tipo ='Delivery Stones' /> 
           <Counter points = {10} tipo ='Delivery Skystones' /> 
@@ -37,9 +36,9 @@ function App() {
         </div>
         
         
-      <div style = {{ float: 'left', textAlign : 'center', display: 'inline-block',width: '40%', height:'100vh'}}>
-      <h1>Driver Controlled</h1>
-        <div >
+      <div  id = 'driverdiv' style = {{ float: 'left', textAlign : 'center', display: 'inline-block',width: '40%', height:'80%'}}>
+      <h1 id = 'driver'>Driver Controlled</h1>
+        <div  >
         <div className = 'drivemode'>
         <Counter points = {1} tipo ='Delivery Stones' /> 
         </div>
@@ -56,10 +55,10 @@ function App() {
        
       </div>
       
-      <div style = {{display : 'inline-block', width: '30%', height:'80%'}}>
+      <div  id = 'enddiv' style = {{display : 'inline-block', width: '30%', height:'80%'}}>
 
-      <h1>End Game</h1>
-        <div>
+      <h1 id = 'endgame'>End Game</h1>
+        <div >
           <div className = 'endgame'>
           <ToggleCount tipo ='Capping 1' puntos = {5}/>
           </div>
--- src/Components/Timer/timer.js
+++ src/Components/Timer/timer.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import Button from 'react-bootstrap/Button'
 import PropTypes from 'prop-types'
+import sonido from '../../static/Sonido2.wav'
+import timbre from '../../static/timbre.wav'
 class Timer extends Component {
 
     constructor(props){
@@ -13,21 +15,79 @@ class Timer extends Component {
 
             tiempo: this.props.tiempo,
             tiempo_form: result,
-            enabled : true
+            enabled : true,
+            clicked :false,
         }
         this.Start = this.Start.bind(this)
         this.Stop = this.Stop.bind(this)
+        this.reset = this.reset.bind(this)
+        this.audio = new Audio(sonido)
+        this.timbre = new Audio(timbre)
+        
+    }
+    reset (){
+        this.audio.pause()
+        let date = new Date(null);
+                                date.setSeconds(this.props.tiempo); // specify value for SECONDS here
+                                let result = date.toISOString().substr(11, 8);
+        this.setState({clicked: false,enabled: false, tiempo : this.props.tiempo, tiempo_form: result})
+        clearInterval(this.timer)
     }
     Start(){
         console.log('Start');
         this.setState({enabled: true})
+
+        let auto = document.getElementById('auto')
+        let driver = document.getElementById('driver')
+        let endgame = document.getElementById('endgame')
+        let enddiv = document.getElementById('enddiv')
+        let autodiv= document.getElementById('autodiv')
+        let drivediv = document.getElementById('driverdiv')
+        if(!this.state.clicked){
+        this.setState({clicked : !this.state.clicked})
+       
         this.timer = setInterval(() =>
                     { 
+                        this.audio.loop =true
+                        this.audio.load()
+                        this.audio.volume=1
+                        this.audio.play()
+                        
                         if(this.state.enabled){
 
 
+                        
+                            if(this.state.tiempo >this.props.lim){
+                                if (this.state.tiempo>this.props.driver){
 
-                            if(this.state.tiempo >0){
+                           
+                                    auto.style.color = '#57FF57'
+                                    autodiv.style.border = 'solid 5px #57FF57 '
+                                    drivediv.style.border = ''
+                                    enddiv.style.border = ''
+                                    
+                                driver.style.color = 'white'
+                                
+                                 endgame.style.color = 'white'
+                                }
+                                else if(this.state.tiempo>this.props.endgame && this.state.tiempo<this.props.driver){
+                                    auto.style.color = 'white'
+                                    drivediv.style.border = 'solid 5px #57FF57 '
+                                    autodiv.style.border = ''
+                                    enddiv.style.border = ''
+                                 driver.style.color = '#57FF57'
+                                    endgame.style.color = 'white'
+                                    
+                                }
+                                else if(this.state.tiempo<this.props.endgame){
+                                    auto.style.color = 'white'
+                                 driver.style.color = 'white'
+                                 drivediv.style.border = ''
+                                 auto.style.border = ''
+                                 enddiv.style.border = 'solid 5px #57FF57 '
+                                    endgame.style.color = '#57FF57'
+                                    
+                                }
                                 let date = new Date(null);
                                 date.setSeconds(this.state.tiempo); // specify value for SECONDS here
                                 let result = date.toISOString().substr(11, 8);
@@ -41,7 +101,12 @@ class Timer extends Component {
                             
                           })}
                           else{
+                              this.audio.pause()
+                              
                               clearInterval(this.timer)
+                            this.timbre.play()
+                              
+                             
                               
                           }
 
@@ -49,7 +114,7 @@ class Timer extends Component {
 
                         }
                    
-                    }, 1000)
+                    }, 1000)}
                 
                 
             
@@ -57,8 +122,10 @@ class Timer extends Component {
     }
     Stop(){
         console.log('Stop');
-        this.setState({enabled : false})
+        this.setState({enabled : false, clicked: false})
         clearInterval(this.timer)
+        this.audio.pause()
+        
         
 
     }
@@ -70,6 +137,7 @@ class Timer extends Component {
             <h1>{this.state.tiempo_form}</h1>
             <Button onClick = {this.Start}>Start</Button>
             <Button onClick = {this.Stop}>Stop</Button>
+            <Button onClick = {this.reset}>Reset</Button>
             </div>
         )
         }
